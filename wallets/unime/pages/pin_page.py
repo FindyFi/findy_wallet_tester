8192f39a1b385db871b300e64a2e7d607b826f3c
Aleksi Autio <aleksi.autio@findy.fi>
2026-07-30 17:13:51 +0300

    Fixed the script with newer release of unime

--- wallets/unime/pages/pin_page.py
+++ wallets/unime/pages/pin_page.py
@@ -9,8 +9,7 @@ from base.utils import wait_present
 # UniMe uses a password (not a digit PIN).  The lock screen heading is "Unlock wallet".
 HEADING = (AppiumBy.XPATH, '//*[@text="Unlock wallet"]')
 
-# Password input on the lock screen.
-_password_input = (AppiumBy.XPATH, '//android.widget.EditText[@hint="Enter your password"]')
+_password_input = (AppiumBy.XPATH, '//android.widget.EditText[@password="true"]')
 
 # The submit button also says "Unlock wallet".
 _unlock_btn = (AppiumBy.XPATH, '//android.widget.Button[@text="Unlock wallet"]')
